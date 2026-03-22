e9f6cd40d038fde1bb72d753a681e6cdf16dbbb2
YumiYamashita <2412110324n@kindai.ac.jp>
2026-03-22 18:14:18 +0900

    暗号鍵を環境変数注入に変更し秘密情報管理を強化

--- fuel/app/config/crypt.php
+++ fuel/app/config/crypt.php
@@ -1,6 +1,27 @@
 <?php
+
+/**
+ * 暗号鍵はリポジトリに固定値を置かず、環境変数から注入する
+ *
+ * 必須:
+ * - FUEL_CRYPTO_KEY
+ * - FUEL_CRYPTO_IV
+ * - FUEL_CRYPTO_HMAC
+ */
+
+$crypto_key = getenv('FUEL_CRYPTO_KEY');
+$crypto_iv = getenv('FUEL_CRYPTO_IV');
+$crypto_hmac = getenv('FUEL_CRYPTO_HMAC');
+
+if (empty($crypto_key) or empty($crypto_iv) or empty($crypto_hmac))
+{
+  throw new RuntimeException(
+    '暗号鍵が未設定です。FUEL_CRYPTO_KEY / FUEL_CRYPTO_IV / FUEL_CRYPTO_HMAC を環境変数に設定してください。'
+  );
+}
+
 return array(
-  'crypto_key' => 'o2Yy04ges3uYICA-084eYHqw',
-  'crypto_iv' => 'X00fOgEaIV3AsCAaF88VEz24',
-  'crypto_hmac' => 'L9wrAwM10ZxYZbsCBMABcjcY',
+  'crypto_key' => $crypto_key,
+  'crypto_iv' => $crypto_iv,
+  'crypto_hmac' => $crypto_hmac,
 );
